e80538e9dc0989a5618453dcf6db2fa42745a283
BenjaminNassW <benjanass@gmail.com>
2022-02-15 20:07:41 +0000

    basico

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for(let i=0; i<=1; i++){
+
+    for(let j=0; j<=1; j++){
+
+        for(let k=0; k<=1; k++){
+            console.log(pronoun[i]+adj[j]+noun[k]+'.com')
+
+}
+}
+}
