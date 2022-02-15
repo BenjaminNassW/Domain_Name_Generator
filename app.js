960f302bc7cdfc112aa02a8853f250207b6c5bef
BenjaminNassW <benjanass@gmail.com>
2022-02-15 20:13:19 +0000

    more domains

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
-
+let domain = ['.com', '.io', '.net', '.us']
 for(let i=0; i<=1; i++){
-
     for(let j=0; j<=1; j++){
-
         for(let k=0; k<=1; k++){
-            console.log(pronoun[i]+adj[j]+noun[k]+'.com')
-
+            for(let l=0; l<=3; l++){
+            console.log(pronoun[i]+adj[j]+noun[k]+domain[l]);
+            }
 }
 }
 }
